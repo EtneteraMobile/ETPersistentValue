0420a19dfbbd22062498ae08484a17434603f7c7
Jan Cislinsky <jan.cislinsky@etnetera.cz>
2017-09-02 20:16:06 +0200

    + adds PersistentDate (swift4)

---
+++ Sources/GeneralTypes/PersistentDate.swift
@@ -0,0 +1,10 @@
+//
+//  PersistentDate.swift
+//  ETPersistentValue iOS
+//
+//  Created by Jan Čislinský on 02/09/2017.
+//
+
+import Foundation
+
+open class PersistentDate: PersistentValue<Date> {}
