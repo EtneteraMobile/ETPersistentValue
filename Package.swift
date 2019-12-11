d8ea597f7c4d97d6f03f01e43795e45feb06f70c
Bohdan Kubkin <bohdan.kubkin@etnetera.cz>
2019-12-11 10:13:22 +0100

    ♻️ Changed project target name to make imports more pretty

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ETPersistentValue",
-            targets: ["ETPersistentValue iOS"])
+            targets: ["ETPersistentValue"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,7 +20,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ETPersistentValue iOS",
+            name: "ETPersistentValue",
             path: "Sources"
         )
     ]
