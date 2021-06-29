16aa37f0ea2b92e87372871e22f8f6e1f5dd383c
Miroslav Beleš <miroslav@beles.cz>
2021-06-29 06:56:34 +0200

    Dynamic library

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,6 +10,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ETPersistentValue",
+            type: .dynamic,
             targets: ["ETPersistentValue"])
     ],
     dependencies: [
