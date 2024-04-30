80e47d5b50726168f422a51f9c491d27cadcad13
Lukáš Ambrož <lukas.ambroz@etnetera.cz>
2024-04-30 16:02:58 +0200

    Added support for SPM static library

--- Package.swift
+++ Package.swift
@@ -9,6 +9,10 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
+            name: "ETPersistentValue-Static",
+            targets: ["ETPersistentValue"]
+        ),
+        .library(
             name: "ETPersistentValue",
             type: .dynamic,
             targets: ["ETPersistentValue"])
